edcdec3ce1f8354a83b56c7fffe45722a842f4c9
d1du <d1du0191@gmail.com>
2024-04-24 22:32:51 -0700

    k8s files

--- client/src/App.js
+++ client/src/App.js
@@ -20,6 +20,7 @@ function App() {
             Learn React
           </a>
           <Link to="/">Home</Link>
+          v2v2v2
           <Link to="/otherpage">Other Page</Link>
         </header>
         <div>
